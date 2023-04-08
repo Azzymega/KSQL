b9589690844c9973c8099d8543397b5f58c650f0
Danil <123>
2023-04-08 21:15:00 +0300

    Костыль №1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -48,6 +48,7 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(0, 28);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(1405, 596);
             this.dataGridView1.TabIndex = 0;
             // 
